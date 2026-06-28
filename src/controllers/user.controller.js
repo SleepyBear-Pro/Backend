1318bcff5929c76205211e57d23d3bda3b13ff14
SleepyBear-Pro <ojasrane36@gmail.com>
2026-06-28 12:34:26 +0530

    done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,14 +17,14 @@ const registerUser = asyncHandler( async (req, res) => {
     // check for user creation
     // return response
     
-    const {fullname, email, username,passowrd} = req.body
-    console.log("email: ", email);
+    const {fullname, email, username,password} = req.body
+    //console.log("email: ", email);
 
 
    
 
     if (
-        [fullname, email, username,passowrd].some((field) => 
+        [fullname, email, username,password].some((field) => 
             field?.trim() === "")
     ) {
         throw new ApiError(400, "All fields are required")
@@ -39,7 +39,15 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+
+
+
 
     if(!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
