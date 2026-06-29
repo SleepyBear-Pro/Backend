4d86c28ce585b25547f5f3491b1ef55c5b12f548
SleepyBear-Pro <ojasrane36@gmail.com>
2026-06-29 12:58:17 +0530

    Implement user account management features: change password, update details, and manage avatars

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -32,7 +32,7 @@ const userSchema = new mongoose.Schema(
             type: String, // cloudinary url
             required : true
         },
-        coverimage: {
+        coverImage: {
             type: String, // cloudinary url
         },
         watchHistory: [
